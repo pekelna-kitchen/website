76119b368dcd0200c7c51f0712db0bb264d36050
Vitalii Poterba <vptrbv@gmail.com>
2022-09-19 02:16:29 +0300

    added products

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class Location(models.Model):
     name = models.TextField('Location')
 
+class Product(models.Model):
+    name = models.TextField('ProductName')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    
--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Location
+from .models import Location, Product
+
+import logging
 
 # Create your views here.
 def index(request):
 
+    productList = []
     locations = Location.objects.all()
 
-    return render(request, "db.html", {"locations": locations})
+    for location in locations:
+        productList.append( (location, Product.objects.filter(location=location.id)) )
+
+    return render(request, "db.html", {"productList": productList})
